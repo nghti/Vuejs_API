d1fd22edea31d6824f4601221b5aa684136e6592
nghti <nghti8020@gmail.com>
2020-03-02 01:28:16 +0700

    Fix co ban

--- frontend/src/views/dashboard/Motel.vue
+++ frontend/src/views/dashboard/Motel.vue
@@ -1,81 +1,133 @@
 <template>
   <div>
-    <h2>DTKD</h2>
-    <a-form :form="form" @submit="handleSubmit">
-      <a-form-item
-        v-for="(k, index) in form.getFieldValue('keys')"
-        :key="k"
-        v-bind="formItemLayout"
-        :label="`Node[${index}]`"
-        :required="false"
-      >
-        <a-input-number
-          :formatter="value => `${value}VND`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-          :parser="value => value.replace(/\VND\s?|(,*)/g, '')"
-          @change="handleFormChange"
-          v-decorator="[
-          `money[${k}]`,
-          {
-            validateTrigger: ['change', 'blur'],
-          },
-        ]"
-          placeholder="passenger name"
-          style="width: 60%; margin-right: 8px"
-        />
-        <a-icon
-          v-if="form.getFieldValue('keys').length > 1"
-          class="dynamic-delete-button"
-          type="minus-circle-o"
-          :disabled="form.getFieldValue('keys').length === 1"
-          @click="() => remove(k)"
-        />
-      </a-form-item>
-      <a-form-item v-bind="formItemLayoutWithOutLabel">
-        <a-button type="dashed" style="width: 60%" @click="add">
-          <a-icon type="plus" /> Add field
-        </a-button>
-      </a-form-item>
-      <a-form-item label="Note-DT" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <h2 v-if="formMe.money">
-          {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) | formatMoney }}VND
-        </h2>
-      </a-form-item>
-      <a-form-item label="Note-T" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-input
-          v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
-        />
-      </a-form-item>
-      <a-form-item label="Note-C" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-input
-          v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
-        />
-      </a-form-item>
-      <a-form-item label="Note-DV" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-input
-          v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
-        />
-      </a-form-item>
-      <a-form-item label="Note-TLG" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-input
-          v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
-        />
-      </a-form-item>
-      <a-form-item label="Note-TL %" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-input
-          v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
-        />
-      </a-form-item>
-      <a-form-item label="Note-QL %" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-        <a-input
-          v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
-        />
-      </a-form-item>
-      <a-form-item v-bind="formItemLayoutWithOutLabel">
-        <a-button type="primary" html-type="submit">
-          Submit
-        </a-button>
-      </a-form-item>
-    </a-form>
+    <h2 class="title">Bản Tính Dòng Tiền Kinh Doanh</h2>
+    <a-tabs defaultActiveKey="1">
+      <a-tab-pane tab="Cơ bản" key="1">
+        <a-form :form="form" @submit="handleSubmit">
+          <a-form-item label="Trệt (MB)" v-bind="formItemLayout" class="label-bold">
+            <a-input-number
+              :formatter="value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+              :parser="value => value.replace(/\VND\s?|(,*)/g, '')"
+              placeholder="Tiền MB dự tính"
+              v-decorator="['mb']"
+              style="width: 90%"
+            />
+          </a-form-item>
+          <a-form-item
+            class="label-bold"
+            v-for="(k, index) in form.getFieldValue('keys')"
+            :key="k"
+            v-bind="formItemLayout"
+            :label="`Phòng ${index+1}`"
+            :required="false"
+          >
+            <a-input-number
+              :formatter="value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+              :parser="value => value.replace(/\VND\s?|(,*)/g, '')"
+              @change="handleFormChange"
+              v-decorator="[
+              `money[${k}]`,
+              {
+                validateTrigger: ['change', 'blur'],
+              },
+            ]"
+              placeholder="Tiền phòng dự tính"
+              style="width: 90%; margin-right: 8px"
+            />
+            <a-icon
+              v-if="form.getFieldValue('keys').length > 1"
+              class="dynamic-delete-button"
+              type="minus-circle-o"
+              :disabled="form.getFieldValue('keys').length === 1"
+              @click="() => remove(k)"
+            />
+          </a-form-item>
+          <a-form-item v-bind="formItemLayoutWithOutLabel" class="label-bold">
+            <a-button type="dashed" style="width: 220px" @click="add">
+              <a-icon type="plus" /> Thêm phòng
+            </a-button>
+          </a-form-item>
+          <a-form-item label="Danh thu" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyDanhthu }} VND / 1 Tháng
+            </h2>
+          </a-form-item>
+          <a-form-item label="Giá thuê" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyGiathue }} VND / 1 Tháng
+            </h2>
+          </a-form-item>
+          <a-form-item label="Tiền cọc với chủ" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyToicoc }} VND / 3 Tháng
+            </h2>
+          </a-form-item>
+          <a-form-item label="Tiền cọc của khách" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyDanhthu }} VND / 1 Tháng
+            </h2>
+          </a-form-item>
+          <a-form-item label="Tổng đầu tư" v-bind="formItemLayout" class="red-h2 label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyTong }} VND
+            </h2>
+          </a-form-item>
+          <a-form-item label="Lợi nhuận rồng" v-bind="formItemLayout" class="blue-h2 label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyThu }} VND / 1 Tháng
+            </h2>
+          </a-form-item>
+          <a-form-item label="Tỷ suất lợi nhuận rồng" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyTyrong }} %
+            </h2>
+          </a-form-item>
+          <a-form-item label="Thời gian hoàn vốn" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) + formMe.mb | formatMoneyHoa }} Tháng
+            </h2>
+          </a-form-item>
+          <a-form-item label="Lợi nhuận trên tổng đầu tư" v-bind="formItemLayout" class="label-bold">
+            <h2 v-if="formMe.money">
+              {{ formMe.money.reduce(function(a, b) { return a + b; }, 0) | formatMoneyLoi }} %
+            </h2>
+          </a-form-item>
+          <!-- <hr>
+          <a-form-item label="Note-C" v-bind="formItemLayout">
+            <a-input
+              v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="Note-DV" v-bind="formItemLayout">
+            <a-input
+              v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="Note-TLG" v-bind="formItemLayout">
+            <a-input
+              v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="Note-TL %" v-bind="formItemLayout">
+            <a-input
+              v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+            />
+          </a-form-item>
+          <a-form-item label="Note-QL %" v-bind="formItemLayout">
+            <a-input
+              v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+            />
+          </a-form-item> -->
+          <a-form-item v-bind="formItemLayoutWithOutLabel">
+            <a-button type="primary" html-type="submit">
+              Tính Tiền
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </a-tab-pane>
+      <a-tab-pane tab="Trung cấp" key="2" forceRender>Content of Tab Pane 2</a-tab-pane>
+      <a-tab-pane tab="Cao cấp" key="3">Content of Tab Pane 3</a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 <script>
@@ -86,18 +138,18 @@
     data: () => ({
       formItemLayout: {
         labelCol: {
-          xs: { span: 24 },
-          sm: { span: 5 },
+          sm: { span: 7 },
+          lg: { span: 7 },
         },
         wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 12 },
+          sm: { span: 17 },
+          lg: { span: 10 },
         },
       },
       formItemLayoutWithOutLabel: {
         wrapperCol: {
-          xs: { span: 24, offset: 0 },
-          sm: { span: 12, offset: 5 },
+          sm: { span: 17, offset: 7 },
+          lg: { span: 10, offset: 7 },
         },
       },
       fields: {},
@@ -129,10 +181,38 @@
       }
     },
     filters: {
-      formatMoney: (value) => {
-        console.log('value', value)
+      formatMoneyDanhthu: (value) => {
         return Number((value).toFixed(1)).toLocaleString()
-      }
+      },
+      formatMoneyGiathue: (value) => {
+        return Number((value*0.7).toFixed(1)).toLocaleString()
+      },
+      formatMoneyToicoc: (value) => {
+        return Number((value*0.7*3).toFixed(1)).toLocaleString()
+      },
+      formatMoneyTong: (value) => {
+        const me = value*0.7*3
+        const Kcoc = value
+        return Number((me - Kcoc).toFixed(1)).toLocaleString()
+      },
+      formatMoneyThu: (value) => {
+        return Number((value - value*0.7).toFixed(1)).toLocaleString()
+      },
+      formatMoneyTyrong: (value) => {
+        const loi = value - value*0.7
+        const tong = value*0.7*3 - value
+        return Number((loi/tong*100).toFixed(2))
+      },
+      formatMoneyHoa: (value) => {
+        const loi = value - value*0.7
+        const coc = value*0.7*3 - value
+        return Number((coc/loi).toFixed(2))
+      },
+      formatMoneyLoi: (value) => {
+        const loi = value - value*0.7
+        const tong = value*0.7*4
+        return Number((loi/tong*100).toFixed(2))
+      },
     },
     methods: {
       ...mapActions('dashboard', ['postForm']),
@@ -140,6 +220,7 @@
         e.preventDefault()
         this.form.validateFields((err, values) => {
           if (!err) {
+            console.log('values', values)
             this.postForm(values)
           }
         })
@@ -157,7 +238,6 @@
         if (keys.length === 1) {
           return
         }
-
         // can use data-binding to set
         form.setFieldsValue({
           keys: keys.filter(key => key !== k),
@@ -181,5 +261,20 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .title {
+    font-size: 30px;
+    margin-bottom: 40px;
+    margin-left: 10px;
+  }
+  /deep/ .label-bold {
+    font-weight: bold;
+  }
+  /deep/ .red-h2 .ant-form-item-label label {
+    color: #FF0000;
+  }
+  /deep/ .blue-h2 {
+    label {
+      color: #00FF00;
+    }
+  }
 </style>
